813a00e3a7750f7b0eb66fc38a53cadb07c3ca2b
Romson Preechawit <r.preechawit@hotmail.com>
2018-03-27 02:01:42 +0700

    update mnistnet to new version of Experiment, have not tested yet

--- experiments/mnistnet.py
+++ experiments/mnistnet.py
@@ -12,10 +12,6 @@ from keras import backend as K
 K.set_image_dim_ordering('th')
 from keras.models import model_from_json
 
-def listdir_nohidden(path):
-	directories = glob.glob(path+'/**/*', recursive=True)
-	return [file for file in directories if os.path.isfile(file)]
-
 class MNISTNET(Experiment):
 
     EXPERIMENT_NAME = "SIMPLE MNISTNET"
@@ -23,52 +19,13 @@ class MNISTNET(Experiment):
     Models trained using a simple network originally intended for MNIST.
     """
 
-    classes = [chr(i) for i in range(ord('ก'), ord('ฮ')+1)]
-    classes_dict = {chr(i):[0,0] for i in range(ord('ก'), ord('ฮ')+1)}
-
     def _model_from_json(self, json, **kwargs):
         return model_from_json(json)
 
-    def predict(self, model, test_sample, **kwargs):
+    def predict(self, model, test_sample, classes, **kwargs):
         pred = model.predict_classes(test_sample)
         pred_class = MNISTNET.classes[pred[0]]
         return pred_class
-
-    def evaluate(self, model, test_samples, **kwargs):
-
-        # { character : [count, right] }
-        classes = MNISTNET.classes
-        classes_dict = MNISTNET.classes_dict
-
-        correct_count = 0
-        test_data_count = 0
-
-        for class_key in test_samples:
-            samples = test_samples[class_key]
-            test_data_count += len(samples)
-            for img in samples:
-                pred = model.predict_classes(img)
-                
-                pred_proba = model.predict_proba(img)
-                pred_proba = "%.2f%%" % (pred_proba[0][pred]*100)
-
-                pred_class = classes[pred[0]]
-
-                is_correct = str(pred_class) == str(class_key)
-
-                classes_dict[class_key][0] += 1
-                if is_correct:
-                    correct_count += 1
-                    classes_dict[class_key][1] += 1
-
-        # -- end outer for --
-
-        # prevent divide by zero so test can continue
-        if test_data_count == 0:
-            test_data_count = -1
-
-        self.general_logger.info('{}/{} correct ({})'.format(correct_count, test_data_count, correct_count/test_data_count))
-        return {k:(classes_dict[k][1]/classes_dict[k][0] if classes_dict[k][0] > 0 else 0) for k in classes_dict}
 
     def _createmodel(self, X_train, y_train, X_test, y_test, batch_size, epochs, **kwargs):
         num_classes = y_test.shape[1]
@@ -87,8 +44,7 @@ class MNISTNET(Experiment):
         model.add(Dense(50, activation='relu'))
         model.add(Dense(num_classes, activation='softmax'))
         # Compile model
-        model.compile(loss='categorical_crossentropy', optimizer='adam', metrics=['accuracy'])
-        return model
+        return self._compilemodel(model, **kwargs)
 
     def _compilemodel(self, model, **kwargs):
         model.compile(loss='categorical_crossentropy', optimizer='adam', metrics=['accuracy'])
